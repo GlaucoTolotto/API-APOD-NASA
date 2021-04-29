8415ee01290817cb69ed6db1bd4d2dc164e558b2
GlaucoTolotto <glaucotolotto@gmail.com>
2021-04-29 17:16:43 -0300

    Implementação da API no codigo

--- api.js
+++ api.js
@@ -1,3 +1,22 @@
-$.ajax() {
+var url = "https://api.nasa.gov/planetary/apod?api_key=vD6zfTkfhpVK5SpaD4VRIHQ5oVU3UT9d2MpTiSX0";
+function principal(){
+var data = $("#colocaData").val();
+    $.ajax({url: url+"&date="+data,
+    success: function(result){
+    mostrarImg(result)
+} 
+})
     
 }
+
+function mostrarImg(obj){
+    $("#imagem").attr("src", obj.url)
+    $("h1").html(obj.title)
+}
+
+
+
+
+
+
+// link api nasa "https://api.nasa.gov/planetary/apod?api_key=vD6zfTkfhpVK5SpaD4VRIHQ5oVU3UT9d2MpTiSX0"
